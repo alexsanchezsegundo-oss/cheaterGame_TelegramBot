07eeb6ed10ec5e3a66726ba490bccd6ba36fb45a
Alex Sanchez <alexsanchezsegundo@gmail.com>
2025-12-31 02:35:43 +0100

    not working, but with new features

---
+++ words.py
@@ -0,0 +1,7 @@
+word_list = [
+    "Pizza", "Hamburguesa", "Sushi", "Chocolate", "Café", "Helado", "Cerveza", "Taco",
+    "Playa", "Hospital", "Cine", "Aeropuerto", "París", "Gimnasio", "Supermercado", "Escuela",
+    "Paraguas", "Gafas de sol", "Guitarra", "Smartphone", "Reloj", "Espejo", "Cámara", "Bicicleta",
+    "Elefante", "Pingüino", "León", "Gato", "Delfín", "Jirafa", "Tiburón", "Perro",
+    "Astronauta", "Bombero", "Doctor", "Cocinero", "Fútbol", "Navidad", "Internet", "Dinero"
+]
